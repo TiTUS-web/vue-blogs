aa876902dd116118e56f69a541f6e19201785f51
diyanchuk <diynchuk@fmf.ru>
2023-01-31 23:53:33 +0400

    implemented vue3-quill in create-post v1 && made data caching in the component

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,9 +1,11 @@
 import {createStore} from 'vuex';
 
 import auth from '@/store/modules/auth';
+import createPost from '@/store/modules/createPost';
 
 export default createStore({
   modules: {
     auth,
+    createPost,
   },
 });
