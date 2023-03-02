e2131a6669eb823cb638ad1607499671008403a5
diyanchuk <diynchuk@fmf.ru>
2023-03-03 00:34:09 +0400

    create search module template

---
+++ src/modules/search/index.vue
@@ -0,0 +1,19 @@
+<script lang="ts">
+import Search from '@/components/ui/Search.vue';
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  components: {
+    Search
+  },
+  setup() {
+    return {};
+  }
+});
+    
+</script>
+
+<template>
+    <Search />
+</template>
+
