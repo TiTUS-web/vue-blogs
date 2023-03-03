c05fdda1b9e053a340f65a4ad62392326caaa43b
TiTUS <us.daniel_roberts@mail.ru>
2023-03-03 04:42:29 +0400

    create not_found module

---
+++ src/modules/not_found/NotFound.vue
@@ -0,0 +1,22 @@
+<script lang="ts">
+import {defineComponent} from 'vue';
+
+export default defineComponent({
+  name: 'NotFound',
+  setup() {
+    return {};
+  }
+});
+</script>
+
+<template>
+  <div class="not_found">
+    <img alt="not_found" class="picture not_found__picture" src="@/assets/not_found.png">
+    <p class="title not_found__title">Что-то пошло не так...</p>
+
+    <router-link class="button not_found__button" to="/">
+      <img alt="arrow" class="icon not_found__icon" src="@/assets/icons/arrow.svg">
+      Вернуться назад
+    </router-link>
+  </div>
+</template>
