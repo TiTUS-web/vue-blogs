4f0ca7a96a5e50a281375988a79cadf5dcbe8177
TiTUS <us.daniel_roberts@mail.ru>
2023-03-04 16:17:54 +0400

    create views of the current category and post

--- src/modules/home/components/Posts.vue
+++ src/modules/home/components/Posts.vue
@@ -7,80 +7,92 @@ export default {
 <template>
   <section class="posts">
     <div class="post">
-        <header class="post-header post__header">
-            <div class="category post-header__category">
-                Категория 1
-            </div>
+      <header class="post-header post__header">
+        <router-link class="category post-header__category" to="/categories/1">
+          Категория 1
+        </router-link>
 
-            <p class="date post-header__date post-header__date--header">
-                13 декабря 2022
-            </p>
-        </header>
+        <p class="date post-header__date post-header__date--header">
+          13 декабря 2022
+        </p>
+      </header>
 
-        <div class="post-content post__content">
-            <p class="title post-content__title">Управление ИТ-активами – скучная рутина или творческая задача?</p>
-            <p class="text post-content__text">
-                Размышляя об управлении ИТ-активами, я вспомнил один учебный пример. Менеджер по ИТ-мощностям в крупной компании периодически готовил толстенный отчёт руководству. В очередной раз он не принёс отчёт, решив проверить, нужен ли он вообще. 
-            </p>
-        </div>
+      <div class="post-content post__content">
+        <router-link class="title post-content__title" to="/posts/1">
+          Управление ИТ-активами – скучная рутина или
+          творческая задача?
+        </router-link>
+        <p class="text post-content__text">
+          Размышляя об управлении ИТ-активами, я вспомнил один учебный пример. Менеджер по ИТ-мощностям в крупной
+          компании периодически готовил толстенный отчёт руководству. В очередной раз он не принёс отчёт, решив
+          проверить, нужен ли он вообще.
+        </p>
+      </div>
 
-        <footer class="post-footer post__footer">
-            <p class="date post-header__date">
-                13 декабря 2022
-            </p>
-        </footer>
+      <footer class="post-footer post__footer">
+        <p class="date post-header__date">
+          13 декабря 2022
+        </p>
+      </footer>
     </div>
 
     <div class="post">
-        <header class="post-header post__header">
-            <div class="category post-header__category">
-                Категория 1
-            </div>
-
-            <p class="date post-header__date post-header__date--header">
-                13 декабря 2022
-            </p>
-        </header>
-
-        <div class="post-content post__content">
-            <p class="title post-content__title">Управление ИТ-активами – скучная рутина или творческая задача?</p>
-            <p class="text post-content__text">
-                Размышляя об управлении ИТ-активами, я вспомнил один учебный пример. Менеджер по ИТ-мощностям в крупной компании периодически готовил толстенный отчёт руководству. В очередной раз он не принёс отчёт, решив проверить, нужен ли он вообще. 
-            </p>
+      <header class="post-header post__header">
+        <div class="category post-header__category">
+          Категория 1
         </div>
 
-        <footer class="post-footer post__footer">
-            <p class="date post-header__date">
-                13 декабря 2022
-            </p>
-        </footer>
+        <p class="date post-header__date post-header__date--header">
+          13 декабря 2022
+        </p>
+      </header>
+
+      <div class="post-content post__content">
+        <p class="title post-content__title">
+          Управление ИТ-активами – скучная рутина или творческая задача?
+        </p>
+        <p class="text post-content__text">
+          Размышляя об управлении ИТ-активами, я вспомнил один учебный пример. Менеджер по ИТ-мощностям в крупной
+          компании периодически готовил толстенный отчёт руководству. В очередной раз он не принёс отчёт, решив
+          проверить, нужен ли он вообще.
+        </p>
+      </div>
+
+      <footer class="post-footer post__footer">
+        <p class="date post-header__date">
+          13 декабря 2022
+        </p>
+      </footer>
     </div>
 
     <div class="post">
-        <header class="post-header post__header">
-            <div class="category post-header__category">
-                Категория 1
-            </div>
-
-            <p class="date post-header__date post-header__date--header">
-                13 декабря 2022
-            </p>
-        </header>
-
-        <div class="post-content post__content">
-            <p class="title post-content__title">Управление ИТ-активами – скучная рутина или творческая задача?</p>
-            <p class="text post-content__text">
-                Размышляя об управлении ИТ-активами, я вспомнил один учебный пример. Менеджер по ИТ-мощностям в крупной компании периодически готовил толстенный отчёт руководству. В очередной раз он не принёс отчёт, решив проверить, нужен ли он вообще. 
-            </p>
+      <header class="post-header post__header">
+        <div class="category post-header__category">
+          Категория 1
         </div>
 
-        <footer class="post-footer post__footer">
-            <p class="date post-header__date">
-                13 декабря 2022
-            </p>
-        </footer>
-    </div>
+        <p class="date post-header__date post-header__date--header">
+          13 декабря 2022
+        </p>
+      </header>
 
+      <div class="post-content post__content">
+        <p class="title post-content__title">
+          Управление ИТ-активами – скучная рутина или творческая задача?
+        </p>
+        <p class="text post-content__text">
+          Размышляя об управлении ИТ-активами, я вспомнил один учебный пример. Менеджер по ИТ-мощностям в крупной
+          компании периодически готовил толстенный отчёт руководству. В очередной раз он не принёс отчёт, решив
+          проверить, нужен ли он вообще.
+        </p>
+      </div>
+
+      <footer class="post-footer post__footer">
+        <p class="date post-header__date">
+          13 декабря 2022
+        </p>
+      </footer>
+    </div>
   </section>
 </template>
 
--- src/router/modules/home.ts
+++ src/router/modules/home.ts
@@ -1,12 +1,24 @@
 import {RouteRecordRaw} from 'vue-router';
 
 import Home from '@/modules/home/index.vue';
+import CurrentPost from '@/modules/home/views/CurrentPost.vue';
+import CurrentCategory from '@/modules/home/views/CurrentCategory.vue';
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
     name: 'home',
     component: Home,
+  },
+  {
+    path: '/posts/:id',
+    name: 'current-post',
+    component: CurrentPost,
+  },
+  {
+    path: '/categories/:id',
+    name: 'current-category',
+    component: CurrentCategory,
   },
 ];
 
